f39702dc92d52cf41ae28d80e11d5ab9057bbbcb
athul_tharol <athultharol1994@gmail.com>
2022-02-28 13:57:26 +0530

    Welcome to Gamabling Stimulator

---
+++ Gambling.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.gambling;
+
+public class Gambling {
+	static void printMessage() {
+		System.out.println("Welcome To Gambling Stimulator!!");
+	}
+	static int firstStake = 100;
+	// $1 for each bet
+	static int betOnEachPay = 1;
+	
+
+	public static void main(String[] args) {
+		printMessage();
+
+	}
+
+}
